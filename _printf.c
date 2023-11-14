175c2f4d0f7a6c8492e7c1008815e67da2966501
pluginxdev <paulolawale@gmail.com>
2023-11-14 08:47:47 -0800

    _printf.c

--- _printf.c
+++ _printf.c
@@ -1,28 +1,42 @@
-#include "holberton.h"
+#include "main.h"
+
 /**
- * _printf - prints according to format
- * @format: The given format
+ * _printf - prints a formatted string to stdout
+ * @format: the formatted string
  *
- * Return: On success 1.
+ * Return: the number of characters printed to stdout,
+ * else -1 if @format is NULL
  */
 int _printf(const char *format, ...)
 {
-	va_list list;
-	int i = 0, length = 0;
+	int char_count = 0;
+	va_list args;
+	string_buffer buffer;
 
-	if (format == NULL || (format[0] == '%' && !format[1]))
+	if (!format)
+	{
 		return (-1);
-	va_start(list, format);
-		for (i = 0; format[i]; i++)
-		{
-			if (format[i] == '%')
-			{
-				length += get_print_func(format[i + 1], list);
-				i++;
-			}
-			else
-				length += _putchar(format[i]);
-		}
-	va_end(list);
-	return (length);
+	}
+
+	/* process variable arguments list */
+	va_start(args, format);
+
+	/* Declare a buffer to store the formatted string */
+	init_buffer(&buffer);
+
+	/* Process format string and store the result in the buffer */
+	char_count = process_format(&buffer, format, args);
+
+	va_end(args);
+	if (char_count == -1)
+	{
+		safefree(buffer.string);
+		return (-1);
+	}
+
+	write_string(buffer.string, char_count);
+
+	safefree(buffer.string);
+
+	return (char_count);
 }
