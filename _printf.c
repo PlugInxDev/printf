0bbc425558bb9bf9b6f145a52a18b4b17f99147c
pluginxdev <paulolawale@gmail.com>
2023-11-14 07:51:27 -0800

    _printf.c

--- _printf.c
+++ _printf.c
@@ -1,70 +1,34 @@
-#include "main.h"
-
-void cleanup(va_list args, buffer_t *output);
-int run_printf(const char *format, va_list args, buffer_t *output);
-int _printf(const char *format, ...);
+#include "holberton.h"
 
 /**
- * cleanup - Performs cleanup for _printf
- * @args: a va_list of arguments to _printf
- * @output: A buffer_t struct
- * cleanup - PErforms cleanup operations for _printf,
- * @args: A va_list of arguments provided to _printf,
- * @output: A buffer_t struct.
- * _printf - a function that produces output according to a format
- */
-
-void cleanup(va_list args, buffer_t *output)
-{
-	va_end(args);
-	write(1, output->start, output->len);
-	free_buffer(output);
-}
-
-/**
- * run_printf - Reads through the format string for _printf
- * @format: Character string to print - may contain directives
- * @output: A buffer_t struct containing a buffer
- * @args: A va_list of arguments
- * run_printf - Reads through the format string for _printf.
- * @format: Character string to print - may contain directives.
- * @output: A buffer_t struct containing a buffer.
- * @args: A va_list of arguments.
+ * _printf - prints according to a given format. printf Implementation.
+ * @format: string holding characters and format of arguments to be printed.
  *
- * Return: The number of characters stored to output
+ * Return: number of charaters printed.
  */
 
-int run_printf(const char *format, va_list args, buffer_t *output)
+int _printf(const char *format, ...)
 {
-	int i, wid, prec, ret = 0;
+	va_list original, copy;
+	int n_printed = 0, error = 0;
 
-	return (ret);
-}
-
-
-/**
- * _primtf - a function that produces output according to a format
- *  _printf _ Outputs a formatted string
- * @format: character string to print - may contain directives
- *
- * Return: The number of characters printed.
- */
-
-int _printf(cons char *format, ....)
-{
-	buffer_t *output;
-	vs_list args;
-	int ret;
-
-	if (format == NULL)
-		return (-1);
-	output = init_buffer();
-	if (output == NULL)
+	if (format == NULL || *format == '\0')
 		return (-1);
 
-	va_start(args, format);
+	va_start(original, format);
+	va_copy(copy, original);
 
-	ret = run_printf(format, args, output);
+	error = check_input(format, &original);
 
-	return (ret);
+	if (error != 0)
+	{
+		return (error * -1);
+	}
+
+	n_printed = print_output(format, &copy);
+
+	va_end(original);
+	va_end(copy);
+
+	return (n_printed);
 }
