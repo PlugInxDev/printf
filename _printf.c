92926a6cd325b8e431cbaca1bbf6c86e956bd359
pluginxdev <paulolawale@gmail.com>
2023-11-14 08:45:59 -0800

    Added function to _memcpy function

--- _printf.c
+++ _printf.c
@@ -1,34 +1,28 @@
 #include "holberton.h"
-
 /**
- * _printf - prints according to a given format. printf Implementation.
- * @format: string holding characters and format of arguments to be printed.
+ * _printf - prints according to format
+ * @format: The given format
  *
- * Return: number of charaters printed.
+ * Return: On success 1.
  */
-
 int _printf(const char *format, ...)
 {
-	va_list original, copy;
-	int n_printed = 0, error = 0;
+	va_list list;
+	int i = 0, length = 0;
 
-	if (format == NULL || *format == '\0')
+	if (format == NULL || (format[0] == '%' && !format[1]))
 		return (-1);
-
-	va_start(original, format);
-	va_copy(copy, original);
-
-	error = check_input(format, &original);
-
-	if (error != 0)
-	{
-		return (error * -1);
-	}
-
-	n_printed = print_output(format, &copy);
-
-	va_end(original);
-	va_end(copy);
-
-	return (n_printed);
+	va_start(list, format);
+		for (i = 0; format[i]; i++)
+		{
+			if (format[i] == '%')
+			{
+				length += get_print_func(format[i + 1], list);
+				i++;
+			}
+			else
+				length += _putchar(format[i]);
+		}
+	va_end(list);
+	return (length);
 }
